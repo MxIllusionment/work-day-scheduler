20835c4f250db604c24915621c44f8e635e39b6a
Steven Israel <malenchite@gmail.com>
2020-12-28 20:49:26 -0500

    Update time-based display on page load and every minute

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,7 @@ function pageInitializer() {
     /* TODO: Dynamically generate formatted time */
     hourCol.text(i);
 
+    mainCol.attr("id", "main-" + i);
     mainCol.addClass("col-9 main-col");
     input.attr("data-hour", i);
     mainCol.append(input);
@@ -34,5 +35,32 @@ function pageInitializer() {
   }
 }
 
+/* Update date and row classes based on current time */
+function timeUpdate() {
+  /* Update date at top of screen */
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+  /* Remove past/present/future classes from all main columns */
+  $(".main-col").removeClass("past present future");
+
+  /* Update each main column based on the current time */
+  for (var i = startTime; i < endTime; i++) {
+    var newClass;
+    var currentHour = moment().hour();
+    if (currentHour > i) {
+      newClass = "past";
+    } else if (currentHour < i) {
+      newClass = "future";
+    } else {
+      newClass = "present";
+    }
+    $("#main-" + i).addClass(newClass);
+  }
+}
+
+/* Update time-based information every minute */
+setInterval(timeUpdate, 60000);
+
 /* Page initialization */
 pageInitializer();
+timeUpdate();
